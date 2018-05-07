e63d674bd9cd83dc817764651928c3654c73c9fd
Alexander Daly <31634797+AlexanderDaly@users.noreply.github.com>
2018-05-07 11:31:23 -0400

    Update 01-simple-linear-regressions.py

--- 15-introduction-to-data-visualization-with-python/03-statistical-plots-with-seaborn/01-simple-linear-regressions.py
+++ 15-introduction-to-data-visualization-with-python/03-statistical-plots-with-seaborn/01-simple-linear-regressions.py
@@ -18,7 +18,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Plot a linear regression between 'weight' and 'hp'
-sns.lmplot(data=auto, x='weight', y='hp')
+sns.lmplot(x='weight', y='hp', data=auto)
 
 # Display the plot
 plt.show()
